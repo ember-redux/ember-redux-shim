01dc2e31e8b647e4ebd4af723fc4a14c216d8332
Robert Wagner <rwwagner90@gmail.com>
2017-03-20 15:01:35 -0400

    Use ember-cli-node-assets

--- blueprints/ember-redux-shim/index.js
+++ blueprints/ember-redux-shim/index.js
@@ -1,19 +1,14 @@
 module.exports = {
-  afterInstall: function () {
-    return this.addPackagesToProject([
-      {name: 'redux', target: '3.6.0'}
-    ])
-      .then(() => {
-        return this.addAddonsToProject({
-          packages: [
-            {name: 'ember-lodash-shim', target: '^2.0.0'},
-            {name: 'ember-symbol-observable', target: '0.1.2'}
-          ]
-        })
-      })
+  afterInstall: function() {
+    return this.addAddonsToProject({
+      packages: [
+        { name: 'ember-lodash-shim', target: '^2.0.0' },
+        { name: 'ember-symbol-observable', target: '0.1.2' }
+      ]
+    })
   },
 
-  normalizeEntityName: function () {
+  normalizeEntityName: function() {
     // this prevents an error when the entityName is not specified
   }
 }
--- index.js
+++ index.js
@@ -1,59 +1,64 @@
 'use strict'
 
-const mergeTrees = require('broccoli-merge-trees')
-const path = require('path')
-const replace = require('broccoli-replace')
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const mergeTrees = require('broccoli-merge-trees');
+const path = require('path');
+const replace = require('broccoli-replace');
 
 module.exports = {
   name: 'redux',
+
+  options: {
+    nodeAssets: {
+      'redux': {
+        vendor: {
+          include: ['dist/redux.js'],
+          processTree(tree) {
+            return replace(tree, {
+              files: '**/*.js',
+              patterns: [
+                {
+                  match: /process\.env\.NODE_ENV/g,
+                  replacement: `"${EmberApp.env()}"`
+                },
+                {
+                  match: /import isPlainObject from 'lodash\/isPlainObject'/g,
+                  replacement: "import lodash from 'lodash'\nconst isPlainObject = lodash.isPlainObject"
+                }
+              ]
+            })
+          }
+        }
+      }
+    }
+  },
 
   /*
    This method climbs up the hierarchy of addons
    up to the host application.
    This prevents previous addons (prior to `this.import`, ca 2.7.0)
    to break at importing assets when they are used nested in other addons.
-  */
-  _findHost: function () {
-    var current = this
-    var app
+   */
+  _findHost: function() {
+    var current = this;
+    var app;
 
     // Keep iterating upward until we don't have a grandparent.
     // Has to do this grandparent check because at some point we hit the project.
     do {
-      app = current.app || app
+      app = current.app || app;
     } while (current && current.parent && current.parent.parent && (current = current.parent))
 
-    return app
+    return app;
   },
 
-  treeForAddon (tree) {
-    const app = this._findHost()
-    const reduxPath = path.dirname(require.resolve('redux/src/index.js'))
-    let reduxTree = this.treeGenerator(reduxPath)
+  included() {
+    this._super.included.apply(this, arguments);
 
-    // Fix import paths to not include ".js" extension in name
-    reduxTree = replace(reduxTree, {
-      files: '**/*.js',
-      patterns: [
-        {
-          match: /process\.env\.NODE_ENV/g,
-          replacement: `"${app.env}"`
-        },
-        {
-          match: /import isPlainObject from 'lodash\/isPlainObject'/g,
-          replacement: "import lodash from 'lodash'\nconst isPlainObject = lodash.isPlainObject"
-        }
-      ]
-    })
+    const app = this._findHost();
 
-    if (!tree) {
-      return this._super.treeForAddon.call(this, reduxTree)
-    }
-
-    const trees = mergeTrees([reduxTree, tree], {
-      overwrite: true
-    })
-
-    return this._super.treeForAddon.call(this, trees)
+    app.import('vendor/redux/dist/redux.js', {
+      using: [{ transformation: 'amd', as: 'redux' }]
+    });
   }
 }

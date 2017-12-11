672a0123eccebc65e31d74426665abd6316f44b2
toran billups <toranb@gmail.com>
2017-12-10 21:10:46 -0600

    DEPENDENCY: bump ember-symbol-observable to v1.0.1

--- blueprints/ember-redux-shim/index.js
+++ blueprints/ember-redux-shim/index.js
@@ -7,7 +7,7 @@ module.exports = {
         return this.addAddonsToProject({
           packages: [
             {name: 'ember-lodash-shim', target: '^2.0.0'},
-            {name: 'ember-symbol-observable', target: '1.0.0'}
+            {name: 'ember-symbol-observable', target: '1.0.1'}
           ]
         })
       })
